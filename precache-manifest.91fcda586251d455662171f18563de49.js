8d5ea0fcdbb4a4c771bb9503f78217060f00242b
Rohit Azad <rohit.azad@TILLT05907.local>
2019-07-03 13:21:19 +0530

    Updates

---
+++ precache-manifest.91fcda586251d455662171f18563de49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744c8dfdc8635952c29c01daaa37d1dd",
+    "url": "/redux-app/index.html"
+  },
+  {
+    "revision": "0af1003628a46f0a16f2",
+    "url": "/redux-app/static/css/main.a1950167.chunk.css"
+  },
+  {
+    "revision": "d206dd4321cbb0dbda1c",
+    "url": "/redux-app/static/js/2.4ebdf5b9.chunk.js"
+  },
+  {
+    "revision": "0af1003628a46f0a16f2",
+    "url": "/redux-app/static/js/main.a5f2473c.chunk.js"
+  },
+  {
+    "revision": "40275f3f3af3ba99529a",
+    "url": "/redux-app/static/js/runtime~main.0b020989.js"
+  }
+]);
